500ace0de78ccf6b89c04bd1cfe7440d0bacfa3b
Håkon Nessjøen <haakon@trippelm.no>
2018-10-10 19:18:47 +0200

    Support for changing page on current surface, or other surfaces

--- index.js
+++ index.js
@@ -25,13 +25,51 @@ instance.prototype.init = function() {
 	self.callbacks = {};
 	self.instances = {};
 	self.active = {};
+	self.pages = {};
 
-	self.CHOICES_INSSTANCES = [];
+	self.CHOICES_INSTANCES = [];
+	self.CHOICES_SURFACES = [];
+	self.CHOICES_PAGES = [];
+
+	self.pages_getall();
+	self.addSystemCallback('page-update', self.pages_update.bind(self));
+
+	self.devices_getall();
+	self.addSystemCallback('devices_list', self.devices_list.bind(self));
 
 	self.instance_save();
 	self.addSystemCallback('instance_save', self.instance_save.bind(self));
 
 	self.status(self.STATE_OK);
+};
+
+instance.prototype.pages_getall = function() {
+	var self = this;
+
+	self.system.emit('get_page', function (pages) {
+		self.pages = pages;
+	});
+};
+
+instance.prototype.pages_update = function() {
+	var self = this;
+
+	// Update dropdowns
+	self.init_actions();
+};
+
+instance.prototype.devices_list = function(list) {
+	var self = this;
+
+	self.devices = list;
+	self.init_actions();
+};
+instance.prototype.devices_getall = function() {
+	var self = this;
+
+	self.system.emit('devices_list_get', function (list) {
+		self.devices = list;
+	});
 };
 
 instance.prototype.instance_save = function() {
@@ -44,10 +82,10 @@ instance.prototype.instance_getall = function(instances, active) {
 	var self = this;
 	self.instances = instances;
 	self.active = active;
-	self.CHOICES_INSSTANCES.length = 0;
+	self.CHOICES_INSTANCES.length = 0;
 
 	for (var key in self.instances) {
-		self.CHOICES_INSSTANCES.push({ label: self.instances[key].label, id: key });
+		self.CHOICES_INSTANCES.push({ label: self.instances[key].label, id: key });
 	}
 
 	self.init_actions();
@@ -102,6 +140,30 @@ instance.prototype.destroy = function() {
 instance.prototype.init_actions = function(system) {
 	var self = this;
 
+	self.CHOICES_SURFACES.length = 0;
+	self.CHOICES_SURFACES.push({
+		label: 'Current surface',
+		id: 'self'
+	});
+	for (var i = 0; i < self.devices.length; ++i) {
+		self.CHOICES_SURFACES.push({
+			label: self.devices[i].type + ' (' + self.devices[i].serialnumber + ')',
+			id: self.devices[i].serialnumber
+		})
+	}
+
+	self.CHOICES_PAGES.length = 0;
+	for (var page in self.pages) {
+		var name = 'Page ' + page;
+		if (self.pages[page].name !== undefined && self.pages[page].name != 'PAGE') {
+			name += ' (' + self.pages[page].name + ')';
+		}
+		self.CHOICES_PAGES.push({
+			label: 'Page ' + page + ' (' + name	+ ')',
+			id: page
+		});
+	}
+
 	self.system.emit('instance_actions', self.id, {
 		'instance_control': {
 			label: 'Enable or disable instance',
@@ -110,8 +172,8 @@ instance.prototype.init_actions = function(system) {
 					 type: 'dropdown',
 					 label: 'Instance',
 					 id: 'instance_id',
-					 default: self.CHOICES_INSSTANCES[0].id,
-					 choices: self.CHOICES_INSSTANCES
+					 default: self.CHOICES_INSTANCES[0].id,
+					 choices: self.CHOICES_INSTANCES
 				},
 				{
 					 type: 'dropdown',
@@ -121,20 +183,50 @@ instance.prototype.init_actions = function(system) {
 					 choices: self.CHOICES_YESNO_BOOLEAN
 				}
 			]
+		},
+		'set_page': {
+			label: 'Set surface to page',
+			options: [
+				{
+					type: 'dropdown',
+					label: 'Surface / controller',
+					id: 'controller',
+					default: 'self',
+					choices: self.CHOICES_SURFACES
+				},
+				{
+					type: 'dropdown',
+					label: 'Page',
+					id: 'page',
+					default: '1',
+					choices: self.CHOICES_PAGES
+				}
+			]
 		}
 	});
 };
 
-instance.prototype.action = function(action) {
+instance.prototype.action = function(action, extras) {
 	var self = this;
 	var id = action.action;
 	var cmd;
 	var opt = action.options;
 
-	console.log(id, ' opts: ', opt);
-
 	if (id == 'instance_control') {
 		self.system.emit('instance_enable', opt.instance_id, opt.enable == 'true');
+	} else if (id == 'set_page') {
+		var surface = opt.controller == 'self' ? extras.deviceid : opt.controller;
+
+		// Change page after this runloop
+		setImmediate(function () {
+			self.system.emit('device_set_page', surface, opt.page);
+		});
+
+		// If we change page while pushing a button, we need to tell the button that we were done with it
+		// TODO: Somehow handle the futile "action_release" of the same button on the new page
+		if (surface == extras.deviceid) {
+			self.system.emit('bank-pressed', extras.page, extras.bank, false, surface);
+		}
 	}
 };
 
