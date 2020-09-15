cb09f9cb4bbc1b4c37561cdb5a1acb19622ef61f
Justin Osborne <justin@eblah.com>
2020-09-15 12:16:19 -0500

    correct day/month

--- index.js
+++ index.js
@@ -37,8 +37,8 @@ function instance(system, id, config) {
 		const hh = (`0${now.getHours()}`).slice(-2);
 		const mm = (`0${now.getMinutes()}`).slice(-2);
 		const ss = (`0${now.getSeconds()}`).slice(-2);
-		const month = (`0${now.getMonth()}`).slice(-2);
-		const day = (`0${now.getDay()}`).slice(-2);
+		const month = (`0${now.getMonth()+1}`).slice(-2);
+		const day = (`0${now.getDate()}`).slice(-2);
 		const hhmm = hh + ":" + mm;
 		const hhmmss = hhmm + ":" + ss
 		self.setVariable('date_y', now.getFullYear());
